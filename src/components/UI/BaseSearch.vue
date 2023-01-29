708ae702f126787e746860eb4dfbf66eabf43acc
DDH <dung.ho@wizeline.com>
2023-01-29 18:24:09 +0700

    Add files

---
+++ src/components/UI/BaseSearch.vue
@@ -0,0 +1,49 @@
+<template>
+  <form>
+    <input
+      type="search"
+      @input="search"
+      :value="searchTerm"
+      placeholder="Filter items"
+    />
+  </form>
+</template>
+
+<!-- <script>
+export default {
+  props: ['searchTerm'],
+  emits: ['search'],
+  methods: {
+    search(event) {
+      this.$emit('search', event.target.value);
+    },
+  },
+};
+</script> -->
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps(['searchTerm']);
+const emit = defineEmits(['search']);
+
+function search(event) {
+  emit('search', event.target.value);
+}
+</script>
+
+<style scoped>
+input {
+  font: inherit;
+  width: 100%;
+  display: block;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus {
+  outline: none;
+  border-color: #00006b;
+  background-color: #eeeeff;
+}
+</style>
